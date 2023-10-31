37cd3ae86807a29b41b15fbbe3e1d5b32d724e73
Asim Zaidi <asim_zaidi@apple.com>
2023-10-31 00:13:43 -0500

    plaid quickstart dumpa

--- client/src/components/Connect.tsx
+++ client/src/components/Connect.tsx
@@ -1,26 +1,55 @@
-import React from "react";
-import { PlaidLink } from "react-plaid-link";
+import React, { useCallback, useState } from "react";
+import axios from "axios";
 
-const PlaidConnectButton = () => {
-  // Plaid Link configuration
-  const plaidConfig = {
-    token: "YOUR_PLAID_PUBLIC_TOKEN", // Replace with your public Plaid token
-    onSuccess: (public_token: string, metadata: any) => {
-      // Handle the successful connection here, e.g., send public_token to your server
-    },
-    onExit: (error: any, metadata: any) => {
-      // Handle exit events (e.g., user closed the Plaid Link window)
-    },
-  };
+import { usePlaidLink, PlaidLinkOnSuccess } from "react-plaid-link";
+
+const client_id = "654071b04b5732001c52f1f1";
+const secret = "bc7a4a6ca8ef7ead85c8e5bc110313";
+
+const Connect = () => {
+  const [token, setToken] = useState<string | null>(null);
+
+  // get link_token from your server when component mounts
+  React.useEffect(() => {
+    const createLinkToken = async () => {
+      const res = await axios.post(
+        "https://cors-anywhere.herokuapp.com/https://sandbox.plaid.com/link/token/create",
+        {
+          client_id,
+          secret,
+          client_name: "calculator app",
+          country_codes: ["US"],
+          language: "en",
+          products: ["auth"],
+        }
+      );
+      console.log("res:", res);
+      if (res.data?.public_token) {
+        setToken(res.data.public_token);
+      }
+    };
+    createLinkToken();
+  }, []);
+
+  const onSuccess = useCallback<PlaidLinkOnSuccess>((publicToken, metadata) => {
+    // send public_token to your server
+    // https://plaid.com/docs/api/tokens/#token-exchange-flow
+    console.log(publicToken, metadata);
+  }, []);
+
+  console.log("token:", token);
+  const { open, ready } = usePlaidLink({
+    token,
+    onSuccess,
+    // onEvent
+    // onExit
+  });
 
   return (
-    <div>
-      <h2>Connect your bank account</h2>
-      <PlaidLink {...plaidConfig}>
-        <button>Connect</button>
-      </PlaidLink>
-    </div>
+    <button onClick={() => open()} disabled={!ready}>
+      Connect a bank account
+    </button>
   );
 };
 
-export default PlaidConnectButton;
+export default Connect;
